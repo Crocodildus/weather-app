48b44f2dd481cbdbd27f11e8bcb50b4cd2cdf956
Olek <olek.pisarek@gmail.com>
2020-10-07 21:24:19 +0200

    fix: delete fetchedDataToHTML call in functionsToLoad.js

--- src/scripts/functions/functionsToLoad.js
+++ src/scripts/functions/functionsToLoad.js
@@ -1,10 +1,8 @@
-import currentWeatherToHTML from './fetchedDataToHTML';
 import getLocation from './getLocation';
 import searchDOM from '../DOM/searchBtnDOM';
 import searchAutoComplete from '../DOM/autocomplete';
 
 const functionsToLoad = () => {
-  currentWeatherToHTML();
   getLocation();
   searchDOM();
   searchAutoComplete();
