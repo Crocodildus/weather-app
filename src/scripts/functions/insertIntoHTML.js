4f76c61ffad834b37cddd5f3ccdf5791f620c4ed
Olek <olek.pisarek@gmail.com>
2020-10-08 07:40:42 +0200

    rewrite arrow if else to normal

--- src/scripts/functions/insertIntoHTML.js
+++ src/scripts/functions/insertIntoHTML.js
@@ -1,8 +1,14 @@
+import addSymbols from './addSymbols';
 
 const insertIntoHtml = (attribute, obj) => {
   const dataSelector = document.getElementById(attribute);
-  dataSelector !== null ? dataSelector.textContent = obj[attribute]
-    : console.error('not there');
+  if (dataSelector !== null) {
+    dataSelector.textContent = (obj[attribute] + addSymbols(attribute));
+  } else if (typeof obj[attribute] === 'object') {
+    console.log(obj[attribute]);
+  } else {
+    return null;
+  }
 };
 
 export default insertIntoHtml;
