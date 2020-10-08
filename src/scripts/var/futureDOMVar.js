b1ff327d2692ae049df88bb1e5992a3e40d9562f
Crocodildus <olek.pisarek@gmail.com>
2020-10-08 19:11:02 +0200

    chore: add selector file for future weather

---
+++ src/scripts/var/futureDOMVar.js
@@ -0,0 +1,13 @@
+const weekDay = document.getElementsByClassName('weekDay');
+const weekIcon = document.getElementsByClassName('icon');
+const weekDesc = document.getElementsByClassName('desc');
+const dayTemp = document.getElementsByClassName('dayTemp');
+const nightTemp = document.getElementsByClassName('nightTemp');
+
+export {
+  weekDay,
+  weekIcon,
+  weekDesc,
+  dayTemp,
+  nightTemp
+};
